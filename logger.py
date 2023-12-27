a26dd2ae57fd9b4c7cd0896875bb38d13f2edabe
Ivan Dolovčak <idolovcak7@gmail.com>
2023-12-27 23:42:38 +0100

    style: change logger output format

--- logger.py
+++ logger.py
@@ -9,13 +9,12 @@ from importlib.util import find_spec as findModule
 
 class Logger:
     colorSupported: bool = findModule("colorama") is not None
-    padAmt: int = 4
 
     class Mode:
-        error = {"color": "\033[31m", "prompt": "ERR"} # red fg
-        info  = {"color": "\033[34m", "prompt": "INFO"} # blue fg
-        ok    = {"color": "\033[32m", "prompt": "OK"} # green fg
-        note  = {"color": "\033[33m", "prompt": "NOTE"} # yellow fg
+        error = {"color": "\033[31m", "prompt": "[ERROR]"} # red fg
+        info  = {"color": "\033[34m", "prompt": "[INFO]"} # blue fg
+        ok    = {"color": "\033[32m", "prompt": "[OK]"} # green fg
+        note  = {"color": "\033[33m", "prompt": "[NOTE]"} # yellow fg
 
     @classmethod
     def colored(cls, text: str, color: str) -> str:
@@ -26,6 +25,6 @@ class Logger:
 
     @classmethod
     def log(cls, mode: dict[str, str], message: str) -> None:
-        formatted = f"[ {mode['prompt'].center(cls.padAmt)} ] {message}"
+        formatted = f"{mode['prompt'].ljust(8)} {message}"
         
         print(cls.colored(formatted, mode["color"]))
